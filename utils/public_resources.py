416a6a7a5552c8db85c300e9b6c5ccbe282c86bb
zadkie1ll <fedoseenkonikits@gmail.com>
2026-04-14 15:31:50 +0300

    offerta final

--- utils/public_resources.py
+++ utils/public_resources.py
@@ -1,4 +1,4 @@
 TELEGRAM_BOT_URL = "https://t.me/Shredder_vps_bot"
 TELEGRAM_CHANNEL_URL = "https://t.me/shredder_vps"
 TELEGRAM_SUPPORT_URL = "https://t.me/ShredderSUPPORT"
-PUBLIC_OFFER_URL = "https://"
+PUBLIC_OFFER_URL = "https://bt.orpheous.ru/public-offer"
